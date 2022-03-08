d73045b7ed8192369bf61ebcecb3a93f51201677
Fay <faypistik@gmail.com>
2022-03-08 12:13:34 +0100

    filtered images to only show the ones with 300px width) also removed the image component

--- code/src/components/Album.js
+++ code/src/components/Album.js
@@ -1,21 +1,30 @@
 import React from 'react'
-import { Artist } from './Artist'
-import { Image } from './Image'
 
+import { Artist } from './Artist'
 
 export const Album = (props) => {
     return (
         <div className='album'>
-                <div className='image'>{props.album.images.map((data)=>{
-                    return <Image images ={data}/>
+            <div className='image'>{props.album.images
+                .filter((data) => {
+                    if (data.width === 300) {
+                        //if filter is true then it keeps the image and goes to the .map
+                        return true
+                    }
+                    // all false filters are removed and are not passed to the .map
+                    return false
+                })
+                .map((data) => {
+                    console.log(data)
+                    return <img src={data.url} />
                 })}</div>
 
-                
+
             <div>Album name: {props.album.name}</div>
 
-            <div className='artists'>{props.album.artists.map((data)=> {
-                    return <Artist artist={data}/>
-                })}</div>
+            <div className='artists'>{props.album.artists.map((data) => {
+                return <Artist artist={data} />
+            })}</div>
         </div>
     )
 }
