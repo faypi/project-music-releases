e55214ec5dc3ec829fd871663bc6c68f7f614426
Fay Pistikozoglou <91823161+faypi@users.noreply.github.com>
2022-03-08 20:43:02 +0100

    Update code/src/components/Icons.js

--- code/src/components/Icons.js
+++ code/src/components/Icons.js
@@ -1,7 +1,7 @@
 import React from "react";
 export const Icons = () => {
   return (
-    <div class="Icons">
+    <div class="icons">
       <img className="heart icon" src="/icons/heart.svg" />
       <img className="play icon" src="/icons/play.svg" />
       <img className="dots icon" src="/icons/dots.svg" />
