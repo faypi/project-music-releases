da04df38f37e520863973635eccaa1b060d89428
Fay <faypistik@gmail.com>
2022-03-09 18:20:55 +0100

    added footer

--- code/src/App.js
+++ code/src/App.js
@@ -14,6 +14,23 @@ export const App = () => {
           return <Album album={data} />;
         })}
       </div>
+      <div class="footer">
+    <div class="footer-left">
+      <p>
+        Made in SE and GR
+      </p>
+    </div>
+    <div class="footer-center">
+      <p>
+        Week 9 Project for <a href="https://www.technigo.io/program">Technigo</a> Spring '22 
+      </p>
+    </div>
+    <div class="footer-right">
+      <p>
+        © Proudly created by Sherin Susan Thomas and Fay Pistikozoglou
+      </p>
+    </div>
+    </div>
     </div>
   );
 };
